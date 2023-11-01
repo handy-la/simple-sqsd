a42b676030b6449b9e498f951facb52718d9387f
Arturo Ojeda <arturo@handy.la>
2023-11-01 15:24:15 -0600

    Adding latest tag

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -138,6 +138,7 @@ func (s *Supervisor) worker() {
 					})
 				}
 
+				// If the status code is 500, also change the message visibility so that it will be retried. Use a visibility timeout of 5 seconds.
 				if res.StatusCode == http.StatusInternalServerError {
 					changeVisibilityEntries = append(changeVisibilityEntries, &sqs.ChangeMessageVisibilityBatchRequestEntry{
 						Id:                msg.MessageId,
